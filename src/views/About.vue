bc897e78b313a27a9c5e66ae6a0874a92e9cf3b1
akshat-creator <akshatjain@gmail.com>
2020-08-27 02:27:38 +0530

    video embed

--- src/views/About.vue
+++ src/views/About.vue
@@ -77,7 +77,7 @@
       <div>
         <iframe
           width="100%"
-          src="https://www.youtube.com/watch?v=ZdKAmLO8RA0&feature=youtu.be"
+          src="https://www.youtube.com/embed/a_q7ZofaKdM"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
